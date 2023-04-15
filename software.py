f1a027e2a8105f76f4514862970a7b3530e6c0c6
shißdo <61821471+shiueo@users.noreply.github.com>
2023-04-16 02:42:56 +0900

    tmp_word show

--- software.py
+++ software.py
@@ -17,48 +17,38 @@ screen, screen_x, screen_y = window.setup()
 clock = pygame.time.Clock()
 
 #######################################################################################################################
-
-wordSet = os.listdir(global_path.get_proj_abs_path("assets/words"))
-wordSet = [i.split("_")[0] for i in wordSet]
-wordSet = list(set(wordSet))
-
-
-for i in range(len(wordSet)):
-    wordSet[i] = [wordSet[i], wordSet[i]]
-
-random.shuffle(wordSet)
-wordSet = wordSet[:global_variables.numOfTests]
-
 #######################################################################################################################
 cross = pygame.image.load(
     global_path.get_proj_abs_path("assets/cross.png")
 ).convert_alpha()
 cross = pygame.transform.smoothscale(cross, (screen_x, screen_y))
 
+wordSet = os.listdir(global_path.get_proj_abs_path("assets/words"))
+
+print(wordSet)
+
 L = []
 for s in wordSet:
     tmp1 = pygame.image.load(
-        global_path.get_proj_abs_path(f"assets/words/{s[0]}_1.png")
+        global_path.get_proj_abs_path(f"assets/words/{s}")
     ).convert_alpha()
     tmp1 = pygame.transform.smoothscale(tmp1, (screen_x, screen_y))
 
-    tmp2 = pygame.image.load(
-        global_path.get_proj_abs_path(f"assets/words/{s[1]}_2.png")
-    ).convert_alpha()
-    tmp2 = pygame.transform.smoothscale(tmp1, (screen_x, screen_y))
+    L.append(tmp1)
 
-    L.append([tmp1, tmp2])
+L = L[:global_variables.numOfTests]
+random.shuffle(L)
+
 
 #######################################################################################################################
 
 mainLoop = 1
-start_tick = pygame.time.get_ticks()
+global_variables.start_tick = pygame.time.get_ticks()
 #######################################################################################################################
 
-TIMER_BOOL = True
 while mainLoop:
     current_tick = pygame.time.get_ticks()
-    delta_tick = current_tick - start_tick
+    delta_tick = current_tick - global_variables.start_tick
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
@@ -74,7 +64,7 @@ while mainLoop:
 
     # fixation cross(random time between 3s-5s)
     screen.blit(cross, (0, 0))
-    word_show(screen, cross, TIMER_BOOL, )
+    word_show.show(screen, cross, delta_tick, L)
 
     # fps
     dt = clock.tick(60)
