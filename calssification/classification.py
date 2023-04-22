574c1fd9fb00961500bb07690f5934286676c728
shißdo <61821471+shiueo@users.noreply.github.com>
2023-04-22 22:48:53 +0900

    d

--- calssification/classification.py
+++ calssification/classification.py
@@ -6,5 +6,5 @@ import mne
 raw_data = mne.io.read_raw_brainvision("./test_2.vhdr")
 print(raw_data.info)
 
-plotted = raw_data.plot(duration=100, n_channels=31)
+plotted = raw_data.plot(duration=200, n_channels=31)
 plotted.savefig('fig.png')
